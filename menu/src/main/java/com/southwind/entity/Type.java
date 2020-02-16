e786011c337ac469b59596b96621f99482efe8d1
ErenChange <2443684349@qq.com>
2020-02-16 18:07:48 +0800

    V1.0

---
+++ menu/src/main/java/com/southwind/entity/Type.java
@@ -0,0 +1,11 @@
+package com.southwind.entity;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class Type {
+    private long id;
+    private String name;
+}
---
+++ menu/src/main/java/com/southwind/repository/TypeRepository.java
@@ -0,0 +1,10 @@
+package com.southwind.repository;
+
+import com.southwind.entity.Menu;
+import com.southwind.entity.Type;
+
+import java.util.List;
+
+public interface TypeRepository {
+    public List<Type> findAll();
+}
