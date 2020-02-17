1025bba4874f18db8e165cab934268c18e6e9b82
ErenChange <2443684349@qq.com>
2020-02-18 01:29:15 +0800

    V1.0

--- menu/src/main/java/com/southwind/controller/MenuHandler.java
+++ menu/src/main/java/com/southwind/controller/MenuHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/account")
+@RequestMapping("/menu")
 public class MenuHandler {
     @Autowired
     private MenuRepository menuRepository;
